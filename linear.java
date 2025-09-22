2913304e9862d9f6c251d9f0598bdd7e2a063898
rohitkumar-code007 <akkamrohitkumar@gmail.com>
2025-09-22 21:55:29 +0530

    linear.java added

---
+++ linear.java
@@ -0,0 +1,22 @@
+public class linear{
+    public static void main(String[] args) {
+        int[] arr= {2,4,6,8,10,12,14};
+        int k=10;  
+        System.out.println(Linearsearch(arr,k)); 
+
+        
+    }
+    static int Linearsearch(int arr[],int k){
+        if (arr.length==0){
+            return -1;
+        }
+        for(int i=0;i<=arr.length;i++){
+            if(arr[i]==k){
+                return i;
+
+            }
+        }
+       return -1;}
+        
+
+    }
